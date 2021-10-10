c5e766f8ccfc73c34424b6b4645ceaf1043e7de2
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-10-10 00:09:45 -0300

    Salvando arquivos com sucesso

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os.path
+import sys
 import random
 
 def gerar_senha(tamanho_senha):
@@ -23,8 +25,23 @@ def gerar_senha(tamanho_senha):
 
     return senha_str
 
-for i in range(0, 50):
-    print(gerar_senha(random.randint(5, 10)))
+
+def salvar_senha(senha):
+    file_name = "senhas.txt"
+    finalidade = input("Qual é a finalidade dessa senha: ")
+
+    if not os.path.exists(file_name):
+        print(f"O arquivo {file_name} não existe...")
+        print("Crie o arquivo na mesma pasta que o programa .py")
+        sys.exit()
+
+    file_open = open(file_name, "a")
+    file_open.write(senha + " -> " + finalidade + "\n")
+    file_open.close()
+    print("Senha salva com sucesso!")
+
+
+salvar_senha(gerar_senha(random.randint(8, 12)))
 
     
     
